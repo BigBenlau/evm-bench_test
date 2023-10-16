84c2eef7425ec826ee94881e7df9583dbb84bb60
dcy4561112 <dcy920609@163.com>
2023-10-16 21:29:38 +0800

    gethupdate

--- runners/geth/runner.go
+++ runners/geth/runner.go
@@ -43,40 +43,76 @@ var cmd = &cobra.Command{
 		callerAddress := common.BytesToAddress(common.FromHex("0x1000000000000000000000000000000000000001"))
 
 		config := params.MainnetChainConfig
-		rules := config.Rules(config.LondonBlock, false)
+		rules := config.Rules(config.LondonBlock, true, 1681338458)
 		defaultGenesis := core.DefaultGenesisBlock()
 		genesis := &core.Genesis{
 			Config:     config,
 			Coinbase:   defaultGenesis.Coinbase,
 			Difficulty: defaultGenesis.Difficulty,
 			GasLimit:   defaultGenesis.GasLimit,
-			Number:     config.LondonBlock.Uint64(),
-			Timestamp:  defaultGenesis.Timestamp,
+			Number:     1681338457,
+			Timestamp:  1681338458,
 			Alloc:      defaultGenesis.Alloc,
 		}
 
 		statedb, err := state.New(common.Hash{}, state.NewDatabase(rawdb.NewMemoryDatabase()), nil)
 		check(err)
+		//fmt.Fprintln(os.Stdout, "hahaha")
+		//fmt.Println("ok!1111")
 
 		zeroValue := big.NewInt(0)
 		gasLimit := ^uint64(0)
 
-		createMsg := types.NewMessage(callerAddress, &zeroAddress, 0, zeroValue, gasLimit, zeroValue, zeroValue, zeroValue, contractCodeBytes, types.AccessList{}, false)
-		statedb.PrepareAccessList(callerAddress, &zeroAddress, vm.ActivePrecompiles(rules), createMsg.AccessList())
+		tx := types.NewTx(&types.AccessListTx{
+			ChainID:  big.NewInt(1),
+			Nonce:    0,
+			To:       &zeroAddress,
+			Value:    zeroValue,
+			Gas:      gasLimit,
+			GasPrice: zeroValue,
+			Data:     contractCodeBytes,
+		})
+
+		var signer types.Signer
+		signer = types.NewEIP2930Signer(big.NewInt(1))
+		signer.Sender(tx)
+		createMsg, _ := core.TransactionToMessage(tx, signer, zeroValue)
+
+		//createMsg := types.NewMessage(callerAddress, &zeroAddress, 0, zeroValue, gasLimit, zeroValue, zeroValue, zeroValue, contractCodeBytes, types.AccessList{}, false)
+		statedb.Prepare(rules, callerAddress, zeroAddress, &zeroAddress, vm.ActivePrecompiles(rules), createMsg.AccessList)
 
 		blockContext := core.NewEVMBlockContext(genesis.ToBlock().Header(), nil, &zeroAddress)
 		txContext := core.NewEVMTxContext(createMsg)
 		evm := vm.NewEVM(blockContext, txContext, statedb, config, vm.Config{})
+
+		fmt.Println("chainconfig:  ", evm.ChainConfig())
 		_, contractAddress, _, err := evm.Create(vm.AccountRef(callerAddress), contractCodeBytes, gasLimit, new(big.Int))
 		check(err)
 
-		msg := types.NewMessage(callerAddress, &contractAddress, 1, zeroValue, gasLimit, zeroValue, zeroValue, zeroValue, calldataBytes, types.AccessList{}, false)
+		//fmt.Fprintln(os.Stdout, "hehehe")
+		//fmt.Println("ok!2222")
+
+		tx1 := types.NewTx(&types.AccessListTx{
+			ChainID:  big.NewInt(1),
+			Nonce:    1,
+			To:       &contractAddress,
+			Value:    zeroValue,
+			Gas:      gasLimit,
+			GasPrice: zeroValue,
+			Data:     calldataBytes,
+		})
+
+		signer.Sender(tx1)
+		msg, _ := core.TransactionToMessage(tx1, signer, zeroValue)
+		//msg := types.NewMessage(callerAddress, &contractAddress, 1, zeroValue, gasLimit, zeroValue, zeroValue, zeroValue, calldataBytes, types.AccessList{}, false)
+
 		for i := 0; i < numRuns; i++ {
 			snapshot := statedb.Snapshot()
-			statedb.PrepareAccessList(msg.From(), msg.To(), vm.ActivePrecompiles(rules), msg.AccessList())
+			statedb.AddAddressToAccessList(msg.From)
+			statedb.AddAddressToAccessList(*msg.To)
 
 			start := time.Now()
-			_, _, err := evm.Call(vm.AccountRef(callerAddress), *msg.To(), msg.Data(), msg.Gas(), msg.Value())
+			_, _, err := evm.Call(vm.AccountRef(callerAddress), *msg.To, msg.Data, msg.GasLimit, msg.Value)
 			timeTaken := time.Since(start)
 
 			fmt.Println(float64(timeTaken.Microseconds()) / 1e3)
