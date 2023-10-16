f5fa7a4c67ef4611e0ac2ab297fd3b2dfe5b2adb
Ben Lao <benlau413@gmail.com>
2023-10-17 03:31:28 +0800

    update geth runner.go use local geth

--- runners/geth/runner.go
+++ runners/geth/runner.go
@@ -6,13 +6,22 @@ import (
 	"os"
 	"time"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/core"
-	"github.com/ethereum/go-ethereum/core/rawdb"
-	"github.com/ethereum/go-ethereum/core/state"
-	"github.com/ethereum/go-ethereum/core/types"
-	"github.com/ethereum/go-ethereum/core/vm"
-	"github.com/ethereum/go-ethereum/params"
+	// "github.com/ethereum/go-ethereum/common"
+	// "github.com/ethereum/go-ethereum/core"
+	// "github.com/ethereum/go-ethereum/core/rawdb"
+	// "github.com/ethereum/go-ethereum/core/state"
+	// "github.com/ethereum/go-ethereum/core/types"
+	// "github.com/ethereum/go-ethereum/core/vm"
+	// "github.com/ethereum/go-ethereum/params"
+
+	"evm-bench/runners/runner-geth/go-ethereum_offical/common"
+	"evm-bench/runners/runner-geth/go-ethereum_offical/core"
+	"evm-bench/runners/runner-geth/go-ethereum_offical/core/rawdb"
+	"evm-bench/runners/runner-geth/go-ethereum_offical/core/state"
+	"evm-bench/runners/runner-geth/go-ethereum_offical/core/types"
+	"evm-bench/runners/runner-geth/go-ethereum_offical/core/vm"
+	"evm-bench/runners/runner-geth/go-ethereum_offical/params"
+
 	"github.com/spf13/cobra"
 )
 
