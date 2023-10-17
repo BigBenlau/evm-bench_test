5c216d7b4a8536b085d9bdd2bd97450b4a648e77
dcy4561112 <dcy920609@163.com>
2023-10-17 11:42:51 +0800

    dcyUpdate

--- gitpush.sh
+++ gitpush.sh
@@ -6,4 +6,5 @@ read -p "请输入您的 GitHub Token: " github_token
 
 git commit -m "dcyUpdate"
 
-git push https://$github_token@github.com/BigBenlau/evm-bench_test.git  main:dcy
+git push https://$github_token@github.com/BigBenlau/evm-bench_test.git main:dcy
+
