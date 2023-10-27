278c2a49cdc281496f40354d48713eb08c1fc812
dcy456 <dcy920609@gmail.com>
2023-10-27 22:42:39 +0800

    erigon/runner.go has been done and the version of erigon has been changed to v2.53.0

--- gitpush.sh
+++ gitpush.sh
@@ -5,5 +5,5 @@ git add .
 
 git commit -m "erigon/runner.go has been done and the version of erigon has been changed to v2.53.0"
 
-git push https://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@github.com/BigBenlau/evm-bench_test.git dcy:dcy
+git push https://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@github.com/BigBenlau/evm-bench_test.git dcy:dcy
 
